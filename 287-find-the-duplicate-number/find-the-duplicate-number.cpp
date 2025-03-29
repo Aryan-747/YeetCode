da3ece269d10f8e886b456ca38f05cfc11af9261
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-03-29 21:57:07 +0530

    Time: 3 ms (68.39%) | Memory: 65.2 MB (40.80%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+    
+        /* Using hashset (O(n) Space) (O(n) Time)
 
         vector<int> hesh(nums.size(),-1);
 
-        for(int i=0 ; i<nums.size() ; i++)
-        {
+           for(int i=0 ; i<nums.size() ; i++)
+            {
             if(hesh[nums[i]]!=-1)
             {
                 return nums[i];
             }
 
             hesh[nums[i]] = i;
-        }
+        } 
 
         return -1;
-        
+
+        */
+
+        // Using most optimized Tortoise and hare approach (O(n) time) (O(1) space)
+
+        int slow = 0;
+        int fast = 0;
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while(slow!=fast);
+
+        slow = 0;
+
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 };
