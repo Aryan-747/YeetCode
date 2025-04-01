bba1fa971694aff253440a58ada784aa83c541b1
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-04-01 22:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (73.11%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,37 +2,32 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
 
-        vector<int> heshmap(26,0);
+        vector<int> hmap(26,0);
 
-        int l=0;
-        int r=0;
-
-        int n=s.length();
-
-        int longest = 0;
-
+        int l =0;
+        int r =0;
+        int longest =0;
+        int n = s.length();
         int maxfreq = 0;
-
         int len = 0;
 
         while(r<n)
         {
-            heshmap[s[r]-'A']++;     
-
+            hmap[s[r]-'A']++;
             len = r-l+1;
-            maxfreq = max(maxfreq,heshmap[s[r]-'A']);   
-            
-            if((len-maxfreq)>k)
-            {
-                heshmap[s[l]-'A']--;
-                l++;
+            maxfreq = max(maxfreq,hmap[s[r]-'A']);
 
+            if(len-maxfreq>k)
+            {
+                hmap[s[l]-'A']--;
+                l++;
                 len = r-l+1;
             }
-            longest = max(longest,len);
-            r++;
 
+            longest = max(len,longest);
+            r++;
         }
+        
 
         return longest;
     }
