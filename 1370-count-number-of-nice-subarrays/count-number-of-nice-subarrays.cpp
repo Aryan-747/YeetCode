798a494151b737e441c9af966b6327ef71abec5f
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-04-01 23:03:55 +0530

    Time: 7 ms (63.17%) | Memory: 71.5 MB (68.73%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -3,50 +3,47 @@ public:
 
     int toofun(vector<int> &nums, int k)
     {
-
         if(k<0)
         {
             return 0;
         }
 
-        int l=0;
-        int r=0;
-        int cnt =0;
-
-        int maxcnt = 0;
-        int n = nums.size();
+        int l =0;
+        int r =0;
+        int n =nums.size();
+        int oddcnt = 0;
+        int numofsubs =0;
 
         while(r<n)
         {
-            if(nums[r]%2 != 0)
+            if(nums[r]%2!=0)
             {
-                cnt++;
+                oddcnt++;
             }
 
-            while(cnt>k)
+            while(oddcnt>k)
             {
                 if(nums[l]%2!=0)
                 {
-                    cnt--;
+                    oddcnt--;
                 }
                 l++;
             }
 
             int len = r-l+1;
-            maxcnt+=len;
+            numofsubs += len;
             r++;
         }
 
-        return maxcnt;
-
-
+        return numofsubs;
     }
+
 
     int numberOfSubarrays(vector<int>& nums, int k) {
 
-        int finol = toofun(nums,k) - toofun(nums,k-1);
+        int fino = toofun(nums,k) - toofun(nums,k-1);
 
-        return finol;
+        return fino;
         
     }
 };
