8b9446abe3f64836e1d381333eed8a2d4a6456fe
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-04-03 02:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (20.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -18,21 +18,21 @@ public:
             {
                 low++;
                 high--;
+                continue;
             }
 
-            else if (nums[low]<=nums[mid])
+            if(nums[low]<=nums[mid])
             {
-                if(target<=nums[mid] && target>=nums[low])
+                if(target>=nums[low] && target<=nums[mid])
                 {
                     high = mid -1;
                 }
-
                 else
                 {
                     low = mid +1;
                 }
             }
-
+            
             else
             {
                 if(target>=nums[mid] && target<=nums[high])
@@ -41,9 +41,10 @@ public:
                 }
                 else
                 {
-                    high = mid-1;
+                    high = mid -1;
                 }
             }
+
         }
 
         return false;
