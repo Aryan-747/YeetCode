5b716068b3600c2bafa99618110c82b3e8dddcdf
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-04-04 20:17:14 +0530

    Time: 6 ms (54.95%) | Memory: 10.8 MB (73.22%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
 
-        int l=0;
-        int r=0;
-        int n=s.length();
-        vector<int> hmap(26,0);
-        int maxfreq = 0;
-        int maxlen = 0;
+            int r=0;
+            int l=0;
+            int n=s.length();
+            int len=0;
+            
+            vector<int> hmap(26,0);
 
+            int maxfreq =0;
+            int maxlen = 0;
 
-        while(r<n)
-        {
-            hmap[s[r]-'A']++;
-
-            maxfreq = max(maxfreq,hmap[s[r]-'A']);
-            int len = r-l+1;
-
-            if(len-maxfreq>k)
+            while(r<n)
             {
-                hmap[s[l]-'A']--;
-                
-                l++;
+
+                hmap[s[r]-'A']++;
+                maxfreq = max(maxfreq,hmap[s[r]-'A']);
+                len = r-l+1;
+
+                if(len-maxfreq>k)
+                {
+                    hmap[s[l]-'A']--;
+                    l++;
+                }
+
+                len = r-l+1;
+                maxlen = max(maxlen,len);
+                r++;
             }
 
-            len = r-l+1;
-            maxlen = max(maxlen,len);
-            r++;                
-        }
-
-        return maxlen;
-        
+            return maxlen;
     }
 };
