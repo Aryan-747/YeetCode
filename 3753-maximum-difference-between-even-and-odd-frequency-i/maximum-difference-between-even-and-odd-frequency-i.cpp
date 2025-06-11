f4a01cb45c75c0ddc6365123392e2294f2d8308c
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-06-12 01:24:32 +0530

    Time: 1 ms (47.42%) | Memory: 9.2 MB (78.56%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,44 +1,26 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int maxDifference(string s) {
-
-        int evenfreq = 0;
-        int oddfreq = 0;
-
-        vector<int> hesh(26,0);
-
-        for(int i=0 ; i<s.length(); i++)
-        {
-            hesh[s[i]-'a']++;
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
         }
 
-        int maxd = INT_MIN;
-        
-        for(int i=0 ; i<s.length()-1; i++)
-        {
-            for(int j=i+1; j<s.length(); j++)
-            {
-                if(hesh[s[i]-'a']%2!=0)
-                {
-                    if(hesh[s[j]-'a']%2 == 0)
-                    {
-                        maxd = max(maxd,hesh[s[i]-'a']-hesh[s[j]-'a']);
-                    }
-                }
-
-                else
-                {
-                    if(hesh[s[j]-'a']%2!=0)
-                    {
-                        maxd = max(maxd,hesh[s[j]-'a']-hesh[s[i]-'a']);
-                    }
-                }
+        int odd = 0;
+        int even = s.length();
+        for (int n : freq) {
+            if (n % 2 == 1) {
+                odd = max(odd, n);
+            } else if (n != 0) {
+                even = min(even, n);
             }
         }
 
-        return maxd;
-
-
-        
+        return odd - even;
     }
 };
