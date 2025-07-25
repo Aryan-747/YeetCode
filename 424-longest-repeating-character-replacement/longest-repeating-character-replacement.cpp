667d55c4867a424da3ebe0a8bd835aed1bf174ab
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-07-25 23:12:40 +0530

    Time: 12 ms (23.21%) | Memory: 11.1 MB (18.80%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,39 +3,44 @@ public:
     int characterReplacement(string s, int k) {
 
         int l = 0;
-        int maxfreq = 0;
         int r = 0;
         int n = s.length();
 
-        unordered_map<char,int> m1; // better time complexity while using unordered
-        int maxlen = 0;
+        unordered_map<char,int> m1; // better time complexity
 
+        int longest = INT_MIN;
+        int maxfreq = 0;
+        int len = 0;
 
         while(r<n)
         {
             m1[s[r]]++;
+            len = r-l+1;
             maxfreq = max(maxfreq,m1[s[r]]);
-            int len = r-l+1;
 
-            if((len-maxfreq)>k)
+            if(len-maxfreq>k)
             {
-                m1[s[l]]--; // removing element from start
+                m1[s[l]]--;
 
                 if(m1[s[l]] == 0)
                 {
                     m1.erase(s[l]);
                 }
-
-                l++; // moving window;
+                l++;
             }
 
             len = r-l+1;
-            maxlen = max(maxlen,len);
+            longest = max(longest,len);
             r++;
         }
 
-        return maxlen;
-        
+        if(longest == INT_MIN)
+        {
+            return -1;
+        }
 
+
+        return longest;
+        
     }
 };
