5931f9652f575cb923993b3578da37767146f82b
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-08-30 00:30:30 +0530

    Time: 4 ms (75.62%) | Memory: 11.5 MB (43.62%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,34 +2,44 @@ class Solution {
 public:
     string minWindow(string s, string t) {
 
-        int m = s.size();
-        int n = t.size();
-
+        int minlen = INT_MAX;
         vector<int> hashmap(256,0);
 
-        for(int i=0 ; i<n ; i++)
+        int n = s.length(); // length of string
+        int m = t.length(); // length of string containing characters to be present
+
+        int l = 0;
+        int r = 0;
+
+        if(n<m) // edge case
+        {
+            return "";
+        }
+
+        // prestoring characters of string t in hashmap
+
+        for(int i=0 ; i<m ; i++)
         {
             hashmap[t[i]]++;
         }
 
-        int minlen = INT_MAX;
-        int sind = -1;
-        int l = 0;
-        int r = 0;
+
+        int sind = -1; // starting index;
         int cnt = 0;
 
-        while(r<m)
+        while(r<n)
         {
-            if(hashmap[s[r]]>0)
+
+            if(hashmap[s[r]]>0) // present in map already
             {
                 cnt++;
             }
-
             hashmap[s[r]]--;
 
-            while(cnt == n)
+
+            while(cnt == m)
             {
-                if(r-l+1<minlen)
+                if((r-l+1)<minlen) // new smaller substring found, so update
                 {
                     sind = l;
                     minlen = r-l+1;
@@ -37,7 +47,7 @@ public:
 
                 hashmap[s[l]]++;
 
-                if(hashmap[s[l]]>0)
+                if(hashmap[s[l]]>0) // got reinserted in map, so reduce count
                 {
                     cnt--;
                 }
@@ -46,7 +56,6 @@ public:
             }
 
             r++;
-
         }
 
         if(minlen == INT_MAX)
