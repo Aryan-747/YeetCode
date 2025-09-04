edd2812c18e098e62596f40efe67648f78844919
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-09-05 00:15:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (88.80%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+
+        int d1 = abs(z-x); // need to ignore negative sign
+        int d2 = abs(z-y);
+
+        if(d1<d2)
+        {
+            return 1;
+        }
+
+        else if(d2<d1)
+        {
+            return 2;
+        }
+
+        else
+        {
+            return 0;
+        }
+        
+    }
+};
