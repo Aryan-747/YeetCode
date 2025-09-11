215a89c843e2257e83e9a65780df1c4a9033a69e
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-09-11 12:20:48 +0530

    Time: 8 ms (85.68%) | Memory: 15.8 MB (78.32%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+
+     bool isvow(char x)
+        {
+            switch(x){
+                case 'a':
+                    return true;
+                case 'e':
+                    return true;
+                case 'i':
+                    return true;
+                case 'o':
+                    return true;
+                case 'u':
+                    return true;
+                case 'A':
+                    return true;
+                case 'E':
+                    return true;
+                case 'I':
+                    return true;
+                case 'O':
+                    return true;
+                case 'U':
+                    return true; 
+                default:
+                    return false;
+            }
+
+        }
+
+
+    string sortVowels(string s) {
+
+        vector<char> vows;
+
+        for(char x : s)
+        {
+            if(isvow(x))
+            {
+                vows.push_back(x);
+            }
+        }
+
+        sort(vows.begin(),vows.end());
+
+        int vid = 0;
+
+        for(int c = 0 ; c<s.length(); c++)
+        {
+            if(isvow(s[c]))
+            {
+                s[c] = vows[vid];
+                vid++;
+            }
+        }
+
+        return s;
+        
+    }
+};
