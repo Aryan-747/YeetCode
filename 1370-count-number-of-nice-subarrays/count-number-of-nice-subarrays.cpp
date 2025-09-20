f92c68452ce3c301191efa1c2a30df08ac766515
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-09-21 03:34:03 +0530

    Time: 0 ms (100.00%) | Memory: 71.5 MB (68.25%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
 
-
     int findans(vector<int> &nums, int k)
     {
         // base case
@@ -13,41 +12,42 @@ public:
         int l = 0;
         int r = 0;
         int n = nums.size();
-        int totnum = 0;
-        int oddcnt = 0;
+        
+        int oddcount = 0;
+        int noofarrays = 0;
 
         while(r<n)
         {
             if(nums[r]%2!=0)
             {
-                oddcnt++;
+                oddcount++;
             }
 
-            while(oddcnt>k)
+            while(oddcount>k)
             {
                 if(nums[l]%2!=0)
                 {
-                    oddcnt--;
+                    oddcount--;
                 }
                 l++;
             }
 
             int len = r-l+1;
-            totnum+=len;
+            noofarrays +=len;
             r++;
         }
 
-        return totnum;
+        return noofarrays;
 
     }
 
 
 
     int numberOfSubarrays(vector<int>& nums, int k) {
-
-        int answer = findans(nums,k) - findans(nums,k-1);
-
-        return answer;
         
+
+        int ans = findans(nums,k) - findans(nums,k-1);
+        return ans;
+
     }
 };
