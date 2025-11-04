dd8d50d1ab4be8783a50db33612245d98bd877f2
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-11-04 20:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (32.47%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int cnt = 0;
+
+        for(int i=s.length()-1 ; i>=0 ; i--)
+        {
+            if(s[i] != ' ')
+            {
+                cnt++;
+            }
+
+            else if(cnt>0)
+            {
+                if(s[i] == ' ')
+                {
+                    break;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
