6c531d240980342774993cb59394cc9bd74037b3
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-11-13 23:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-      int count = 0;
-
-      for(int i=0 ; i<nums.size(); i++)
-      {
-        if(nums[i]!=0)
-        {
-            swap(nums[i],nums[count]);
-            count++;
-        }
+        int j=0;  // Pointer to place the next non-zero element
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]!=0){
+                // Swap current element with the element at index j 
+                swap(nums[i],nums[j]);
+            
+                j++;  // Move j to the next index for placing non-zero
+            }
         }
     }
 };
